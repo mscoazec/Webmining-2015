7ccab9ac849a0c5eac23476bdde93eaf9d289af8
mscoazec <m.scoazec@gmail.com>
2015-10-13 12:17:50 +0200

    Ajout du code dans le dossier web-crawling.

---
+++ dirbot-master-crawling/dirbot/pipelines.py
@@ -0,0 +1,19 @@
+import unicodedata
+
+class FilterPipeline(object):
+    """A pipeline for filtering out items which contain certain words in their
+    description"""
+
+    # put all words in lowercase
+
+    def process_item(self, item, spider):
+        item['prix'] = item['prix'].replace(u"\u00A0", "")
+        item['prix'] = item['prix'].replace(u"\u20ac", "")
+        item['prix'] = item['prix'].strip()
+        item['url'] = item['url'].replace(u"\u00A0", "")
+        # suppression des espaces multiples
+        item['description'] = ' '.join(item['description'].split())
+        item['description'] = unicodedata.normalize('NFKD',item['description']).encode('ascii','ignore')
+        item['titre'] = ' '.join(item['titre'].split())
+        item['titre'] = unicodedata.normalize('NFKD',item['titre']).encode('ascii','ignore')
+        return item
