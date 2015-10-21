28f2d6c90bacd31f276eff49d7f8b4a13e85905c
mscoazec <m.scoazec@gmail.com>
2015-10-21 23:20:11 +0200

    Visualisation des caractéristiques du training set.
    Et voilà le commit pour les fonctions de tracé (prix en fonction de
    surface, arrondissement etc...), j'ai fait des boîtes à moustache pour
    la visualisation (for now en python).

--- data-analysis/data-analysis.py
+++ data-analysis/data-analysis.py
@@ -11,6 +11,7 @@ from pylab import *
 import matplotlib.pyplot as plt
 
 filename = "apparts_cinq_sites_10-21_traite.csv"
+datename = "-10-21"
 
 r =csv.reader(open(filename,"rb"),delimiter=',')
 x = list(r)
@@ -20,16 +21,75 @@ data = x[1:]
 
 data = np.array(data)
 
-# string vide to 0 pour cast
+# string vide to 0 pour cast to float
 data[data=='']='0'
 
 data = data.astype(np.float)
 
-# prix en fonction de la surface
+# fonction pour tracer un parametre en fonction de l'autre
 
-y = data[:,labels.index('prix')]
-x = data[:,labels.index('surface')]
-plt.scatter(x, y)
-plt.show()
+def traceNuage(xname, yname):
+    
+    plt.figure(figsize=(20,10))
+    
+    y = data[:,labels.index(yname)]
+    x = data[:,labels.index(xname)]
+    
+    plt.scatter(x, y)
+    plt.xlim(0, max(x))
+    plt.ylim(0, max(y))
+    plt.xlabel(xname)
+    plt.ylabel(yname)
+    title(yname+' en fonction de '+xname+' sur le training set')
+    
+    plt.savefig(yname+'_'+xname+datename+'.png')
+
+# fonction pour tracer un parametre en fonction d'un parametre discret
+
+def traceMoustache(xname, yname):
+    
+    defini = 1
+    
+    plt.figure(figsize=(20,10))
+    
+    y = data[:,labels.index(yname)]
+    x = data[:,labels.index(xname)]
+    
+    unique_x = np.unique(x)
+    split_y = []
+    
+    for i in range(len(unique_x)):
+        split_y.append(y[x == unique_x[i]])
+    
+    split_y = np.array(split_y)
+    
+    plt.boxplot(split_y, positions = unique_x, patch_artist = True)
+    
+    if defini:
+        plt.ylim(0, 3500000)
+    else:
+        plt.ylim(0, max(y))
+    
+    plt.xlabel(xname)
+    plt.ylabel(yname)
+    title(yname+' en fonction de '+xname+' sur le training set')
+    
+    plt.savefig(yname+'_'+xname+datename+'_box'+'.png')
 
 # nombre de donnees par arrondissement
+
+# prix en fonction de la surface
+
+traceNuage('surface','prix')
+
+# prix en fonction de l'arrondissement
+
+traceMoustache('arrondissement','prix')
+
+# prix en fonction de la présence de balcon
+
+traceMoustache('balcon','prix')
+
+# prix en fonction de la présence d'ascenseur
+
+traceMoustache('ascenseur','prix')
